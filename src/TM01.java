80061377f2f3f246e3eed2e87fb91f3c0d4ee2ed
ThinkPad X1 Carbon <muhammaddaffa463@gmail.com>
2023-03-25 11:40:21 +0700

    Tugas TM-1

---
+++ src/TM01.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class TM01 {
+    private JPanel Farmasi;
+    private JTextField tableDataTextField;
+    private JTextField hargaTertinggiTextField;
+    private JButton submitButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
